b6930ec30b7b89db1a2a251957a7965e4366a9eb
Fany Ervansyah <fanyervansyah.9c@gmail.com>
2019-12-18 04:42:35 +0700

    feat(sort-date): sorting date

--- src/pages/logExpenditure/index.tsx
+++ src/pages/logExpenditure/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useCallback } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  RefreshControl,
+  ScrollView
+} from "react-native";
 import axios from "axios";
 
 interface expenditure {
@@ -11,48 +18,121 @@ interface expenditure {
 
 interface LogExpenditureState {
   expenditures: Array<expenditure>;
+  isRefreshing: boolean;
 }
 
 class LogExpenditure extends React.Component<{}, LogExpenditureState> {
   state: LogExpenditureState = {
-    expenditures: []
+    expenditures: [],
+    isRefreshing: true
   };
 
-  handleDeletePress = () => {};
+  handleDeletePress = (id: string) => {
+    axios
+      .delete(
+        `https://daily-expanditure-backend.herokuapp.com/expenditure/${id}`
+      )
+      .catch(err => {
+        alert(err);
+        this.setState(
+          {
+            isRefreshing: true
+          },
+          this.getData
+        );
+      })
+      .then(() => {
+        alert("data berhasil dihapus!");
+        this.setState(
+          {
+            isRefreshing: true
+          },
+          this.getData
+        );
+      });
+  };
 
-  componentDidMount = () => {
+  getData = () => {
     axios
       .get("https://daily-expanditure-backend.herokuapp.com/expenditure")
       .then(res => {
-        this.setState({
-          expenditures: res.data
-        });
+        this.setState(
+          {
+            expenditures: res.data,
+            isRefreshing: false
+          },
+          () => {
+            let expendituresCopy = this.state.expenditures;
+
+            //mengurutkan berdasarkan tahun
+            expendituresCopy.sort((a: expenditure, b: expenditure) => {
+              let exA = a.date.split("-");
+              let exB = b.date.split("-");
+              return parseInt(exA[1]) - parseInt(exB[1]);
+            });
+
+            this.setState({
+              expenditures: expendituresCopy
+            });
+          }
+        );
       });
+  };
+
+  componentDidMount = () => {
+    this.getData();
   };
 
   render() {
     return (
-      <View>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            onRefresh={this.getData}
+            refreshing={this.state.isRefreshing}
+          />
+        }
+      >
         {this.state.expenditures.map((expenditure, index) => (
           <View key={`${index}+${expenditure.name}`}>
-            <View style={style.item}>
-              <Text
-                style={{
-                  fontWeight: "bold"
-                }}
-              >
-                {expenditure.name}
-              </Text>
-              <Button
-                title={"delete"}
-                color="#ff3014"
-                onPress={this.handleDeletePress}
-              />
+            {/* mengidentifikasi apakah tanggal sama dengan expenditure sebelumnya.
+            jika iya, maka tidak perlu mengeluarkan header baru. */}
+            {index === 0 ? (
+              <View>
+                <Text
+                  style={{
+                    fontWeight: "bold"
+                  }}
+                >
+                  {expenditure.date}
+                </Text>
+              </View>
+            ) : expenditure.date !== this.state.expenditures[index - 1].date ? (
+              <>
+                <Text>{`\n`}</Text>
+                <Text
+                  style={{
+                    fontWeight: "bold"
+                  }}
+                >
+                  {expenditure.date}
+                </Text>
+              </>
+            ) : null}
+
+            <View>
+              <View style={style.item}>
+                <Text>{expenditure.name}</Text>
+                <Button
+                  title={"delete"}
+                  color="#ff3014"
+                  onPress={() => this.handleDeletePress(expenditure._id)}
+                />
+              </View>
             </View>
-            <Text>{`\n`}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -61,7 +141,7 @@ const style = StyleSheet.create({
   item: {
     borderWidth: 1,
     borderColor: "#d6d6d6",
-    padding: 3,
+    paddingBottom: 1,
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
